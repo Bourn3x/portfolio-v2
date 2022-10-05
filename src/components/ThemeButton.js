006e29c557b03f36661d21c64d4158fd307d0b95
Julian Chong <julianchong10@gmail.com>
2022-10-05 16:29:17 +0800

    :wrench: FIX: bug where personal links only worked if you clicked the text, not the button

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -1,8 +1,10 @@
 export default function ThemeButton({ label, className, redirectUrl }) {
   return (
-    <button
-      className={`rounded-lg border-2 px-4 py-2 font-bold text-lg duration-300 ${className}`}>
-      <a href={redirectUrl} target="_blank" rel="noopener noreferrer">{label}</a>
-    </button>
+    <a href={redirectUrl} target="_blank" rel="noopener noreferrer">
+      <button
+        className={`rounded-lg border-2 px-4 py-2 font-bold text-lg duration-300 ${className}`}>
+        {label}
+      </button>
+    </a>
   )
 }
